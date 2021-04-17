4cd3651d2c889929849ad4a9cd1de1c87185c9c0
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-04-17 13:30:20 +0100

    small improvement

--- 20/20.py
+++ 20/20.py
@@ -1,9 +1,14 @@
+from functools import reduce
+
+
 def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
 
 
 def sum_digits(n):
-    return 0 if n == 0 else n % 10 + sum_digits(n // 10)
+    if n == 0:
+        return 0
+    return reduce(lambda quocient, remainder: remainder + sum_digits(quocient), divmod(n, 10))
 
 
 print(sum_digits(factorial(100)))
