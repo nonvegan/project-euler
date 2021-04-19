6d60a98a268df719781d99ad1fb15bd08114edc6
Pedro Magalhães <49410762+nonvegan@users.noreply.github.com>
2021-04-19 13:07:17 +0100

    more primes

---
+++ 10.py
@@ -0,0 +1,21 @@
+def get_primes_up_to_n_using_list(n):
+    prime_candidates = [False] * 2 + [True] * (n - 2)
+    for i in range(2, n):
+        if prime_candidates[i]:
+            for j in range(2 * i, n, i):
+                prime_candidates[j] = False
+    return [candidate for candidate, is_prime in enumerate(prime_candidates) if is_prime]
+
+
+def get_primes_up_to_n_using_set(n):
+    primes = set(range(2, n))
+    for prime_candidate in primes.copy():
+        if prime_candidate in primes:
+            for remove_prime in range(2 * prime_candidate, n, prime_candidate):
+                if remove_prime in primes:
+                    primes.remove(remove_prime)
+    return primes
+
+
+print(sum(get_primes_up_to_n_using_list(2000000)))
+print(sum(get_primes_up_to_n_using_set(2000000)))  # 3.5x slower
